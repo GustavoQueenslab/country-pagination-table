053cbfe449de5df21fc3ddffc42414e8ce2f93a6
GustavoQueenslab <gustavo@queenslab.pt>
2023-09-26 11:58:26 +0100

    feat: word animation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,23 @@
 export default function Main() {
+  const w1anim = {
+    animation: "w1anim 20s infinite",
+  };
+
+  const w2anim = {
+    animation: "w2anim 20s infinite",
+  };
+
+  const w3anim = {
+    animation: "w3anim 20s infinite",
+  };
+
+  const w4anim = {
+    animation: "w4anim 20s infinite",
+  };
+
+  const w5anim = {
+    animation: "w5anim 20s infinite",
+  };
   return (
     <div className="h-screen bg-water flex flex-col justify-center items-center bg-no-repeat  bg-cover">
       <img src="innofuse.svg" className="absolute top-4 z-30 w-36" />
@@ -6,7 +25,12 @@ export default function Main() {
       <div className="max-w-[1400px] w-full z-40">
         <div className="mx-[10%] md:mx-[15%] flex flex-col gap-1  flex-1">
           <p className="text-white text-5xl font-semibold md:text-8xl">
-            One <span className="text-[#41BDEB]">opportunity</span>
+            One
+            <span className="bg-red-400 container">
+              <span className="word w1 text-[#41BDEB]">World</span>
+              <span className="word w2 text-[#41BDEB]">Ocean</span>
+              <span className="word w3 text-[#41BDEB]">Opportunity</span>
+            </span>
           </p>
           <p className="text-white font-extrabold text-5xl md:text-9xl md:self-end">
             Let&apos;s grab it.
