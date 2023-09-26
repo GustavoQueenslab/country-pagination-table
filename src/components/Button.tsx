2fba7d2ded0c695f9da2cd5eb4f4ca64e51deefe
GustavoQueenslab <gustavo@queenslab.pt>
2023-09-26 16:53:00 +0100

    fix: change button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
   return (
-    <button className="bg-[#00AEEF] mt-14 font-semibold text-white rounded-md py-3.5 px-6">
+    <button className="bg-[#016CB9] mt-14 font-semibold text-white rounded-md py-3.5 px-6">
       {children}
     </button>
   );
