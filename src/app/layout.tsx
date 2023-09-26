a94183b0d3788fb6faca76df943a0f287e291620
GustavoQueenslab <gustavo@queenslab.pt>
2023-09-26 16:43:18 +0100

    feat:  metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Innofuseblue Portugal",
+  applicationName: "Innofuseblue Portugal",
+  description:
+    "Innofuse Blue is a consortium dedicated to building a global ecosystem to lead blue and sustainable innovation projects in Portugal and beyond",
 };
 
 export default function RootLayout({
