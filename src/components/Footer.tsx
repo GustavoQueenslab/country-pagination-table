8e1261a4197d9eaa1ac7efb887778a6bdc22882b
GustavoQueenslab <gustavo@queenslab.pt>
2023-09-27 13:49:34 +0100

    fix: update footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-12 bg-black text-white title-lg xl:display-sm flex justify-center items-center font-normal">
+    <footer className="py-12 bg-[#48484B] text-white body-md xl:body-lg flex justify-center items-center font-normal">
       Innofuseblue Portugal
     </footer>
   );
