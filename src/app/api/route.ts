f92800b9db5f8611091a140d00574d7c76c445bb
GustavoQueenslab <gustavo@queenslab.pt>
2023-10-25 10:39:01 +0100

    fix: update email value

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 import { ServerClient } from "postmark";
 
 const postmarkClient = new ServerClient(process.env.POSTMARK_TOKEN!);
+
 export async function POST(request: Request) {
   const email = JSON.parse(request.headers.get("body")!);
 
   const data = {
     From: "info@queenslab.se",
-    To: "gustavo@queenslab.pt",
+    To: "contact@innofuseblue.com",
     Subject: email.name,
     TextBody: email.message,
     HtmlBody: `<html><p style="margin-top:4">Name: ${email.name}</p><p style="margin-top:4">Company: ${email.company}</p><p style="margin-top:4">Email: ${email.email}</p><p style="margin-top:4">${email.message}</p></html>`,
